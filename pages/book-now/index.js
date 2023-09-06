9513e0a0623f0851751c68b98f1b8ec6791342ee
sebin <sebin@fegno.com>
2023-09-06 10:10:52 +0530

    Changes

--- pages/book-now/index.js
+++ pages/book-now/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BookNow = () => {
   return (
     <div style={{minHeight:"50vh",display:"flex",justifyContent:"center",alignItems:"center",backgroundColor:"rgb(226 238 255)"}}>
-      <h3 className='title-big'>Coming soon</h3>
+      <h3 className='title-big'>Site under maintenance</h3>
     </div>
   )
 }
